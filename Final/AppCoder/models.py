5df017fd2bf70343e15822b71eb67187acea1692
eljoel333 <joel.gasparhernandez@gmail.com>
2023-09-07 00:12:36 -0600

    agregando vistas

--- Final/AppCoder/models.py
+++ Final/AppCoder/models.py
@@ -6,3 +6,9 @@ from django.db import models
 class Curso(models.Model):
     nombre = models.CharField(max_length=50)
     comision = models.IntegerField();
+
+class Mensaje(models.Model):
+    emisor = models.CharField(max_length=30)
+    receptor = models.CharField(max_length=30)
+    mensaje = models.CharField(max_length=100)
+    leido = models.BooleanField()
